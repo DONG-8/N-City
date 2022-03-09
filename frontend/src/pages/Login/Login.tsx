71e7d3771d3036302d0b12a5ff9db0a780643259
장영남 <ggpp0909@naver.com>
2022-03-09 17:51:48 +0900

    [S06P22E106-204] feature-FE : 로그인 페이지 디자인

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,24 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import MetaMaskOnboarding from '@metamask/onboarding'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import MetaMaskOnboarding from "@metamask/onboarding";
+import MetamaskLogo from "../../essets/images/metamask_logo.png"
+import LoginBackground from "../../essets/images/login_background.png"
 
 
+//// style
 const Wrapper = styled.div`
+  background-image: linear-gradient( rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8) ), url(${LoginBackground});
+  font-family: 'Noto Sans KR', sans-serif;
+  display: flex;
+  flex-direction: column;
   justify-content: "center";
   align-items: "center";
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Container = styled.div`
+  margin-left: 20vw;
+`;
+const Header = styled.div`
+  /* display: flex; */
+  /* margin-left: 10vw; */
 `
-const LoginButton = styled.button`
-  
+
+const H1 = styled.h1`
+  font-size: 50px;
+  margin-top: 20vh;
+  margin-bottom: 0;
+`
+
+const H3 = styled.h3`
+  font-size: 30px;
+  margin-top: 0;
+`
+
+const Logo = styled.img`
+  height: 35px;
+  width: auto;
+  margin-right: 10px;
+`
+
+const Button = styled.button`
+  font-family: 'Noto Sans KR', sans-serif;
+  background-color: white;
+  margin-top: 5vh;
+  width: 280px;
+  height: 60px;
+  border: 1px solid black;
+  border-radius: 2vh;
+  font-size: 27px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 
+//// component
 const Login = () => {
+  console.log(LoginBackground)
   const [onboardButtonText, setOnboardButtonText] = useState<string>("");
-  const [buttonDisabledState, setButtonDisabledState] =
-    useState<boolean>(false);
   const [account, setAccount] = useState<string>("");
   useEffect(() => {
-    initialize()
+    initialize();
   }, []);
 
   const { ethereum } = window;
@@ -26,36 +72,36 @@ const Login = () => {
   const forwarderOrigin =
     currentUrl.hostname === "localhost" ? "http://localhost:3000" : undefined;
 
-  const onboarding = new MetaMaskOnboarding({ forwarderOrigin })
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
 
   const initialize = () => {
     MetaMaskClientCheck();
-  }
+  };
 
   // 브라우저에 메타마스크가 깔려있는지 확인하는 함수
   const isMetaMaskInstalled = () => {
     return Boolean(ethereum && ethereum.isMetaMask);
   };
-  
+
   // 메타마스크 설치 여부에 따라 텍스트 변경
   const MetaMaskClientCheck = () => {
     if (!isMetaMaskInstalled()) {
-      setOnboardButtonText("메타마스크 설치")
+      setOnboardButtonText("메타마스크 설치");
     } else {
-      setOnboardButtonText("메타마스크 연결")
+      setOnboardButtonText("메타마스크 연결");
     }
   };
 
   // 메타마스크 설치 여부에 따라 onClick함수 변경(설치 or 연결)
   const onClickButton = () => {
     if (!isMetaMaskInstalled()) {
-      onClickInstall()
+      onClickInstall();
     } else {
-      onClickConnect()
+      onClickConnect();
     }
-  }
+  };
 
-  // 메타마스크 설치하는 페이지로 보냄
+  // 메타마스크 확장 프로그램 설치하는 페이지로 보냄
   const onClickInstall = () => {
     onboarding.startOnboarding();
   };
@@ -63,23 +109,40 @@ const Login = () => {
   // 메타마스크 연결
   const onClickConnect = async () => {
     try {
-      await ethereum.request({ method: 'eth_requestAccounts' });
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
-      accounts[0] ? setAccount(accounts[0]) : setAccount("계정을 불러올 수 없습니다.")
+      await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      accounts[0]
+        ? setAccount(accounts[0])
+        : setAccount("계정을 불러올 수 없습니다.");
     } catch (error) {
       console.error(error);
-      alert("메타마스크에 연결중 오류가 발생하였습니다.")
+      alert("메타마스크에 연결중 오류가 발생하였습니다.");
     }
   };
-  
+
   return (
     <Wrapper>
-      {account}
-      <LoginButton onClick={onClickButton} disabled={buttonDisabledState}>
-        {onboardButtonText}
-      </LoginButton>
+      <Container>
+        {isMetaMaskInstalled() ? (
+          <Header>
+            <H1>지갑에 연결하세요 !</H1>
+            <H3>MetaMask를 이용하여 로그인 할 수 있습니다 !</H3>
+          </Header>
+        ) : (
+          <Header>
+            <H1>지갑이 없으신가요 ?</H1>
+            <H3>지금 바로 생성하세요 !</H3>
+          </Header>
+        )}
+        <p>{account}</p>
+
+        <Button onClick={onClickButton}>
+          <Logo src={MetamaskLogo} alt="Logo" />
+          {onboardButtonText} &rarr;
+        </Button>
+      </Container>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Login;
