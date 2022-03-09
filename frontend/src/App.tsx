dca24e9a19c03ef4a824e77d8cc1edb3349cc022
장영남 <ggpp0909@naver.com>
2022-03-09 23:18:29 +0900

    [S06P22E106-205] feature-FE : 뱃지 유형 선택 디자인

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { Admin, FAQ, Main, Mypage } from './pages/index';
 import Login from './pages/Login/Login';
+import Apply from './pages/Apply/Apply';
 import GlobalStyle from './styles/global';
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/faq" element={<FAQ />}/>
           <Route path="/admin" element={<Admin />}/>
           <Route path="/mypage" element={<Mypage />}/>
+          <Route path="/apply" element={<Apply />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
