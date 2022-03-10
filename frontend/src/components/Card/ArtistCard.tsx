af34d791d191c5054f0546615ea7ab14afcc79b6
bbnerino <bbnerino@gmail.com>
2022-03-11 02:42:46 +0900

    [S06P22E106-210] feature-FE: 순회가능 & 페이지 넘어가기 수정

--- frontend/src/components/Card/ArtistCard.tsx
+++ frontend/src/components/Card/ArtistCard.tsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const verifiedIMG = ()=>{
-  return(
-  <img alt='verified' style={{"height":'1.5rem'}}
-  src='https://cdn-icons.flaticon.com/png/512/5253/premium/5253963.png?token=exp=1646808025~hmac=c2a100c15f1e3a55617c2dfcea482212'/>)
+interface Iprops{
+  artist:{
+    name:string,
+    profileImg:string,
+    verified:boolean,
+    sumnailImg:string,
+    descreption:string
+  }
 }
-
 
 const Cards = styled.div`
   cursor: pointer;
   height: 450px;
   width: 500px;
-  background-color: #fbe8e8;
+  background-color: #ffffff;
   border-radius: 10px;
   box-shadow:1px 3px 7px  ;
   margin: 30px ;
-  .sumnail_image{
-    img{
+  &:hover{
+    &{
+      transform:scale(1.01) ;
+    }
+  }
+`
+const SumnailImg = styled.div`
+  img{
     width:500px;
     height: 250px ;
     object-fit: cover;    
     border-radius: 10px 10px 0 0 ;
     }
-  }
-  .profile_img{
+`
+const ProfileImg = styled.div`
     margin-left: 215px ;
     margin-top:-40px ;
     img{
@@ -33,10 +42,10 @@ const Cards = styled.div`
     border-radius:100%;
     box-shadow:1px 3px 7px  ;
     }
-  }
-  .card_bottom{
-    text-align:center ;
-    .title{
+`
+const CardBottom = styled.div`
+  text-align:center ;
+    .name{
       font-size:1.5rem;
       font-weight:1000 ;
     }
@@ -48,37 +57,31 @@ const Cards = styled.div`
       margin-top:1rem ;
       width: 90% ;
     }
-  }
-  &:hover{
-    &{
-      transform:scale(1.01) ;
-    }
-  }
 `
 
-const ArtistCard = () => {
+const ArtistCard:React.FC<Iprops> = ({artist}) => {
   return (<>
     <Cards>
-        <div className='sumnail_image'>
+        <SumnailImg>
           <img alt="pic" 
-          src='https://lh3.googleusercontent.com/-DTjHlPuR3xIcAKtczuKcknn5NzjKeUk9mh1VB0a01-Vzcu2Xe6DKeDvfNbdx2aBJMv4in0DxJrcnGdiqe-1UVmjzzEowi43TTFJ=w307'/>
-        </div>
-        <div className='profile_img'>
+          src={artist.sumnailImg}/>
+        </SumnailImg>
+        <ProfileImg>
           <img alt="pic"
-          src='https://lh3.googleusercontent.com/rPB_SZcWuxqlK5M6LpQdF_-4gm3ucQ4xuS7AMjgZJk1kseF2d20Q1GTsXPQs_aOyu8iyDpwKowjw1tw1XfyJbna5oeOSJz1n3LAEQZE=w352' />
-        </div>
-        <div className='card_bottom'>
-          <div className='title'>
-            Happy Club 
-            {verifiedIMG()}
+          src={artist.profileImg} />
+        </ProfileImg>
+        <CardBottom>
+          <div className='name'>
+            {artist.name}
+            {artist.verified &&
+            <img alt="verified" style={{"height":'1.5rem'}} src= "/essets/images/verified.png" />}
           </div>
-          <div className='enterprise'>
-          </div>
+          
 
           <div className='description'>
-            Happy Club is the best Club all over the world. u know what? it is better to run out when i see u. cuase i'm too strong to keep ur house safe.
+            {artist.descreption}
           </div>
-        </div>
+        </CardBottom>
         
         
       </Cards>
--- frontend/src/pages/NFTStore/DetailItem.tsx
+++ frontend/src/pages/NFTStore/DetailItem.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { verifiedIMG } from '../../components/Card/ArtistCard'
-const etherURL:string = 'https://cdn-icons.flaticon.com/png/128/4125/premium/4125334.png?token=exp=1646808226~hmac=06a9e13c5df4ab039435da22a37e4463'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ItemCard from '../../components/Card/ItemCard';
+
+
+const etherURL = '/essets/images/ethereum.png'
 const DetailPage = styled.div`
-  background-color: yellow ;
   width: 1500px ;
   margin: auto ;
-  .top{
-    margin-top: 10vh ;
-    display: flex ;
-    justify-content:space-around ;
-  }
 `
-const ItemCard = styled.div`
+const DetailItemCard = styled.div`
   background-color: #fffffe;
   border-radius:10px ;
   box-shadow:1px 3px 7px ;
-  width: 500px;
-  height: 550px;
+  margin-left: 3vw ;
+  width: 600px;
+  height: 650px;
   img{
-    width:500px ;
-    height:500px ;
+    width: 600px;
+    height:600px ;
     border-radius:10px 10px 0 0 ;
   }
   .like{
@@ -30,26 +31,45 @@ const ItemCard = styled.div`
     font-size:1.5rem ;
   }
 `
+const Top = styled.div`
+  margin-top: 10vh ;
+  display: flex ;
+  justify-content:space-around ;
+  `
+const Center = styled.div`
+  
+`
 const Description = styled.div`
   background-color: #fffffe;
   box-shadow:1px 3px 7px ;
   width: 500px;
   height: 550px;
   border-radius: 10px ;
-  .artist{
-    font-weight:1000 ;
+  margin-right: 3vw ;
+  margin-top:5vh ;
+
+`
+const Transaction = styled.div`
+  background-color: #fffffe;
+  box-shadow:1px 3px 7px ;
+  width: 83.5%;
+  margin: auto ;
+  margin-top: 10vh ;
+  border-radius: 10px ;
+`
+const Artist = styled.div`
+  font-weight:1000 ;
     color:#EB6767;
     font-size: 2rem ;
     margin:1rem ;
-  }
-  .title{
-    font-weight:1000 ;
+`
+const Title = styled.div`
+  font-weight:1000 ;
     font-size: 3rem ;
     margin:1rem ;
-
-  }
-  .owner{
-    font-weight:1000 ;
+`
+const Owner = styled.div`
+  font-weight:1000 ;
     display:flex ;
     font-size:1.5rem ;
     margin: 1rem;
@@ -58,9 +78,9 @@ const Description = styled.div`
       margin-left:1rem ;
       color:#EB6767;
     }
-  }
-  .contents{
-    margin:1rem ;
+`
+const Contents = styled.div`
+  margin:1rem ;
     margin-top:2rem ;
     font-weight:1000; ;
     font-size:1.2rem ;
@@ -69,17 +89,12 @@ const Description = styled.div`
       font-size:1.5rem ;
       margin-bottom:2rem ;
     }
-    .content{
-      display: flex;
-      justify-content: space-between ;
-      
-    }
     .red_text{
       color:#EB6767;
     }
-  }
-  .buy{
-    font-size:2rem ;
+`
+const Buy = styled.div`
+  font-size:2rem ;
     font-weight:1000 ;
     display:flex ;
     margin : 1rem;
@@ -104,81 +119,187 @@ const Description = styled.div`
         }
       }
     }
-  }
 `
-const Transaction = styled.div`
-  background-color: #fffffe;
+const Content =styled.div`
+  display: flex;
+      justify-content: space-between ;
+      
+`
+const Bottom = styled.div`
+  
+`
+const ArtistMore = styled.div`
+  h1{
+    margin-top:2rem;
+    margin-left:2rem;
+  }
+  background-color: #fbe9e1;
   box-shadow:1px 3px 7px ;
   width: 83.5%;
-  height: 550px;
+  height: 70vh ;
   margin: auto ;
   margin-top: 10vh ;
   border-radius: 10px ;
+  overflow-y:hidden;
+  overflow-x:scroll;
+  &::-webkit-scrollbar{width: 4px; height:12px;}
+  &::-webkit-scrollbar-thumb{ background-color: #f68383; border-radius: 10px; } 
+  &::-webkit-scrollbar-track{ background-color: #fbe9e1;}
+  margin-bottom: 5vh ;
 `
+const Cards = styled.div`
+  display: flex ;
+`
+const EachTransaction = styled.div`
+  display: flex;
+  justify-content: space-between ;
+  div{
+    flex:2;
+    font-size:1.3rem;
+  }
+`
+const Head = styled.div`
+  display: flex;
+  justify-content: space-between ;
+  div{
+    flex :2;
+    font-weight:1000 ;
+    font-size:1.5rem;
+  }
+`
+interface IProps{
+  item :{
+    name:string,
+    title:string,
+    price:number,
+    liked:number,
+    url:string
+  }[],
+}
 
 const DetailItem = () => {
-  const transactions:{event:string,price?:number, from?:string,to?:string, date:number}[]= [
-    {event:'transfer', from:"59912",to:"24923", date:20220309 },
+  const [transactions,setTransactions] = useState([
+    {event:'transfer', from:"59912",to:"24923", date:20220309},
     {event:'sale',price:1.04, from:"59912",to:"24923", date:20220305 },
     {event:'list',price:1.01, date:20220306 },
-    {event:'minted', date:20220301 },
-  ]
+    {event:'minted', date:20220301 }
+  ])
+  const [items,setItems] = useState<IProps["item"]>([
+    {name:"Hong Hosus",title:"#Hong1535",price:1.24,liked:35,url:'https://lh3.googleusercontent.com/MmtavcUNNiTpLFfDqqol8pwp1_TKSEv0AbkKSxmN2lffhgYtkxAdfAo72lZVSJ4hpRW87s9TCL-HYMEIpaJ8PdgWBQWVlPsMZkgM6A=w305'},
+    {name:"Giks Home",title:"#ghe23434",price:1.35,liked:43,url:'https://lh3.googleusercontent.com/qGLA-qtTThUV063ueH3gLxZgm0pC1VKusEYh7BrOUi8hBMAbssWvv2Vt0oRTdsWO51CDCkvF5Lc93fC62iI_liTxKz1H2qYyQxnRfg=w352'},
+    {name:"Giks Home",title:"#ghe254334",price:1.2,liked:24,url:'https://lh3.googleusercontent.com/BqScg3QwKPcNW_cxtvBws2D2cE8Us-QsN9yYmB_8UzUikBwLfOC5Nc2JgXWOB2ijx4lAU2KcYplGujimb2FUD9ArixBFeCyNPcES=w352'},
+    {name:"Hong Hosus",title:"#Hong1535",price:1.24,liked:35,url:'https://lh3.googleusercontent.com/MmtavcUNNiTpLFfDqqol8pwp1_TKSEv0AbkKSxmN2lffhgYtkxAdfAo72lZVSJ4hpRW87s9TCL-HYMEIpaJ8PdgWBQWVlPsMZkgM6A=w305'},
+    {name:"Giks Home",title:"#ghe23434",price:1.35,liked:43,url:'https://lh3.googleusercontent.com/qGLA-qtTThUV063ueH3gLxZgm0pC1VKusEYh7BrOUi8hBMAbssWvv2Vt0oRTdsWO51CDCkvF5Lc93fC62iI_liTxKz1H2qYyQxnRfg=w352'},
+    {name:"Giks Home",title:"#ghe254334",price:1.2,liked:24,url:'https://lh3.googleusercontent.com/BqScg3QwKPcNW_cxtvBws2D2cE8Us-QsN9yYmB_8UzUikBwLfOC5Nc2JgXWOB2ijx4lAU2KcYplGujimb2FUD9ArixBFeCyNPcES=w352'},
+    {name:"Giks Home",title:"#g53434",price:1.37,liked:52,url:'https://lh3.googleusercontent.com/Op3TUf8vqznY1geantykLx86mlGf4yEaBfKT25utQlQu8keA9ywYdwYYzVSqGZG_3uSJKCNcUBAVK6qs520xhZ6lsP3dVDGsM9wnRA=w352'},
+  ])
+  const item = JSON.parse(localStorage.getItem("item")||"")
+  console.log(item)
   return (
       <DetailPage>
-        <div className='top'>
-          <ItemCard>
-            <img alt ="pic" src='https://lh3.googleusercontent.com/m3ItIEvgov0fZVFGX8suBYdLTk9hTNKb2DNPvWFHnTxpIx-6D_kzR6c1uqXDssIITM9MI3eGkuyXU7ojyxRoCpWK0QOOU2rEciE3Tw=w600'/>
-            <div className='like'> ❤ 32</div>
-          </ItemCard>
+        <Top>
+          <DetailItemCard>
+            <img alt ="pic" src={item.url}/>
+            <div className='like'> ❤ {item.liked}</div>
+          </DetailItemCard>
           <Description>
-            <div className='artist'>
-              ShowMaker {verifiedIMG()}
-            </div>
-            <div className='title' >
-            Hidden Weirdo
-            </div>
-            <div className='owner'>
-              owner: <div className='owner_name'> ParkSos0321</div>
-            </div>
+            <Artist>
+            {item.name}   
+            {item.verfied&&        
+              <img alt="verified" style={{"height":'1.5rem'}} src= "/essets/images/verified.png" />}
+            </Artist>
+            <Title>
+            {item.title}
+            </Title>
+            <Owner>
+              owner: <div className='owner_name'>{item.name}</div>
+            </Owner>
             <hr/>
-            <div className='contents'>
+            <Contents>
               <div className='des_title'>
               작품설명
               </div>
-              <div className='content' >
+              <Content>
                 <span> Contract Adderess : </span> 
                 <span className='red_text'>0x1234515re932</span>
-              </div>
-              <div className='content'>
+              </Content>
+              <Content>
                 <span> Token ID: </span> 
                 <span className='red_text'>8055</span>
-              </div>
-              <div className='content'>
+              </Content>
+              <Content>
                 <span> Token Standard: </span> 
                 <span className='red_text'>ERC-721</span>
-              </div>
-            </div>
+              </Content>
+            </Contents>
             <hr/>
-            <div className='buy'>
+            <Buy>
               <div className='price'>
-                1.04 <img style={{"height":"2rem"}}  alt="이더" src= {etherURL}/>
+                {item.price} 
+                <img style={{"height":"2rem"}}  alt="이더" src= {etherURL}/>
               </div>
               <button className='buy_button'>
                 <img  alt="cart"
                 src='https://cdn-icons-png.flaticon.com/512/3737/3737151.png'/>
-                구매
-              
+                구매   
               </button>
-            </div>
+            </Buy>
           </Description>
-        </div>
-        <div className='center'>
+        </Top>
+        <Center>
           <Transaction>
-            <h1>거래내역</h1>
+              <Accordion defaultExpanded={true} >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <h1>거래내역</h1>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Head>
+                   
+                    <div>event</div>
+                    <div>price</div>
+                    <div>from</div>
+                    <div>to</div>
+                    <div>date</div>
+                  </Head>
+                  {transactions.map((tran,i)=>{
+                  return(
+                    <div key={i}>
+                    <hr/>
+                    <EachTransaction >
+                    <div>{tran.event}</div>
+                    <div>
+                      {tran.price}
+                      {tran.price?<img style={{"height":'1rem'}} alt="ether" src={etherURL}/>:""}
+                      
+                    </div>
+                    <div>{tran.from}</div>
+                    <div>{tran.to}</div>
+                    <div>{tran.date}</div>
+                    {/* a year ago 로 바꾸기 */}
+                    </EachTransaction>
+                    </div>
+                  )}
+                  )}
+                </AccordionDetails>
+              </Accordion>
           </Transaction>
-        </div>
-        <div className='bottom'>
-        </div>
+        </Center>
+        <Bottom>
+          <ArtistMore>
+            <div style={{display:'flex'}}>
+              <h1 style={{color:'#EB6767'}}>{item.name}</h1>
+              <h1 style={{marginLeft:"1rem"}}>의 작품 보기</h1>
+            </div>
+            <Cards>
+            {items.map(item=>{return(
+                <ItemCard key={item.url} item={item} />)})}
+            </Cards>
+          </ArtistMore>
+        </Bottom>
       </DetailPage>
   )
 }
