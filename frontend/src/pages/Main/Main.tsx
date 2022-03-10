3601f4f2fb928cb07e0e5bd438459b09738de0a7
박동준 <qkrehdwns96@naver.com>
2022-03-10 17:46:30 +0900

    [S06P22E106-201] feature-FE : 서브 캐러셀 완료
    이 캐러셀은 조회수 랭킹을 받아 올 공간이다

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -23,7 +23,7 @@ const MainBannerWrapper = styled.div`
   width: 1223px;
   height: 700px;
   color : white;
-  margin: -500px 20px 0 auto;
+  margin: -500px 10px 0 auto;
 `
 
 const MainBanner = styled.div`
@@ -71,27 +71,95 @@ const MainPagenationBanner = styled.div`
     align-items: center;
     /* transform: translate(-250px) */
   }
-
-
   button {
     width : 50px;
     color: white;
   }
 `
-const SubBanner = styled.div`
+const SubBannerWrraper = styled.div`
   position: relative;
   width:448px;
   height:566px;
   background-color: black;
   color : white;
-  margin: -400px auto 0 20px;
+  margin: -500px auto 0 10px;
 `
+
+const SubBanner = styled.div`
+  width: 448px;
+  height: 280px;
+  object-fit:cover;
+  display: flex;
+  flex-direction: column ;
+  flex-wrap: wrap;
+  position: relative;
+  overflow: hidden;
+  z-index:0;
+  background-color: white;
+  .inner {
+    width: 448px;
+  }
+  .inner  img {
+    width : 100%;
+    height: 280px;
+    /* height: 100%; */
+    object-fit: cover;
+  }
+`
+
+const SubPagenationBanner = styled.div`
+  position: absolute;
+  width: 448px;
+  height: 30px;
+  display: flex;
+  justify-content: flex-end;
+  margin: -30px 0 auto;
+  /* background-color: rgba(0,0,0,0.5); */
+  color: white;
+  .container {
+    width : 448px;
+    text-align: center;
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+    justify-content : flex-start;
+    align-items: center;
+    overflow: hidden;
+  }
+  .inner {
+    width : 250px;
+    min-width:250px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* transform: translate(-250px) */
+  }
+
+
+  button {
+    background-color: rgba(0,0,0,0.5);
+    width : 80px;
+    color: white;
+  }
+`
+
+const SubBottomItem = styled.div`
+  width:100%;
+
+`
+
 
 export default function Main() {
   const [position, setPosition] = useState<number>(0)
   const [eventNumber,setEventNumber] = useState<number>(0)
   const [pageTextPosition, setPageTextPosition] = useState<number>(0)
   const [check, setCheck] = useState<number>(0)
+
+  // 오늘의 방 (투데이 많은 방기준!)
+  const [subPosition, setSubPosition] = useState<number>(0)
+  const [subEventNumber,setSubEventNumber] = useState<number>(0)
+  const [subCheck, setSubCheck] = useState<number>(0)
+
   // 나중에 DB 기반의 데이터 형성시켜주고 이미지 받아오기 아니면 그냥 여기서 사용
   const images = [
     { pic: "https://nimage.g-enews.com/phpwas/restmb_allidxmake.php?idx=5&simg=2020121617202605478e0eaf3841f218144160198.jpg", ID: 1 , name :'구찌'},
@@ -106,6 +174,12 @@ export default function Main() {
     { pic: "https://cdn-lostark.game.onstove.com/2022/event/220223_package_j54QvSXfeG3n/images/pc/@img_index.jpg", ID: 2 , name : '로아 프레딧 룩' },
     { pic: "https://cdn-lostark.game.onstove.com/uploadfiles/banner/93964914d8904123a71323313b1a95ba.jpg", ID: 3 , name : '로아 도화가' },
   ];
+
+  const subImages = [{ pic: "https://nimage.g-enews.com/phpwas/restmb_allidxmake.php?idx=5&simg=2020121617202605478e0eaf3841f218144160198.jpg", ID: 1 , name :'구찌'},
+  { pic: "https://cdn-lostark.game.onstove.com/2022/event/220223_package_j54QvSXfeG3n/images/pc/@img_index.jpg", ID: 2 , name : '로아 프레딧 룩' },
+  { pic: "https://cdn-lostark.game.onstove.com/uploadfiles/banner/93964914d8904123a71323313b1a95ba.jpg", ID: 3 , name : '로아 도화가' },]
+
+
   const moveRight = () => {
     // console.log(e)
     // clearTimeout(1000)
@@ -245,14 +319,58 @@ export default function Main() {
       setPageTextPosition(pageTextPosition - 250)
       }
     // console.log(eventNumber,'우')
-    if (check === 0) {setTimeout(() => {setCheck(1)},5000)}
-    else {setTimeout(() => {setCheck(0)},5000)}    
+    if (check === 0) {setTimeout(() => {setCheck(1)},4000)}
+    else {setTimeout(() => {setCheck(0)},4000)}    
   }
 
   useEffect(() => {
     moveAuto()
   },[check])
 
+  useEffect(()=> {
+    moveSubAuto()
+  },[subCheck])
+
+
+  const moveSubAuto = () => {
+    const len = subImages.length
+    const idx = Math.floor((subEventNumber+1)%len)
+    setSubEventNumber(idx)
+    if (idx === 0) {
+      setSubPosition(0)
+    } else {
+      setSubPosition(subPosition - 448)
+    }
+    if (subCheck === 0) {setTimeout(() => {setSubCheck(1)},8000)}
+    else {setTimeout(() => {setSubCheck(0)},8000)}   
+
+  }
+  
+  const moveSubRight = () => {
+    const len = subImages.length
+    const idx = Math.floor((subEventNumber+1)%len)
+    setSubEventNumber(idx)
+    if (idx === 0) {
+      setSubPosition(0)
+    } else {
+      setSubPosition(subPosition - 448)
+    }
+  }
+
+  const moveSubLeft = () => {
+    const len = subImages.length;
+    const idx = Math.floor((subEventNumber-1)%(len))
+    if (0 === subPosition) {
+      setSubPosition((len-1)*(-448))
+    } else {
+      setSubPosition(subPosition + 448)
+    }
+    if (idx < 0) {
+      setSubEventNumber(len-1)
+    } else {
+      setSubEventNumber(subEventNumber-1)
+    }
+  }
 
 
 
@@ -278,7 +396,7 @@ export default function Main() {
             <div className='container'>
               {images.map((value,idx) => {
                   const id = String(idx)
-                  //transform: `translate(${pageTextPosition}px)`, transition: `transform 0.5s`
+                  //transform: `translate(${sub}px)`, transition: `transform 0.5s`
                   if (idx === eventNumber) {
                     return(
                       <div className='inner' id={id} onClick={(e)=>{clickToMove(e)}} style={{ color : 'white',transform: `translate(${pageTextPosition}px)`, transition: `transform 0.5s`}}>{value.name}</div>
@@ -294,7 +412,27 @@ export default function Main() {
             <button>전체 이벤트 목록</button>
           </MainPagenationBanner>
         </MainBannerWrapper>
-        <SubBanner>subBanner</SubBanner>
+        <SubBannerWrraper>
+          <SubBanner>
+            {subImages.map((value,idx) => {
+              // style={{transform: `translate(${position}px)`, transition: `transform 0.5s`}}
+                return(
+                  <div className='inner'>
+                    <img src={value.pic} key={idx+value.name} alt='사진없노'
+                    style={{transform: `translate(${subPosition}px)`, transition: `transform 0.5s`}}/>
+                  </div>  
+                )
+              })}
+          </SubBanner>
+          <SubPagenationBanner>
+            <button onClick={()=>{moveSubLeft()}}>왼쪽</button>
+            <button>{subEventNumber+1}/{subImages.length}</button>
+            <button onClick={()=>{moveSubRight()}}>오른쪽</button>
+          </SubPagenationBanner>
+          <SubBottomItem>하이염</SubBottomItem>
+        </SubBannerWrraper>
+        
+          
       </MainWrapper>
     </>
   );
