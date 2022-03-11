8e3d083147a91e56ab7f5748736ea06a704330ee
김지슬 <wltmfdl123@naver.com>
2022-03-11 18:16:37 +0900

    [S06P22E106-45] Feat : Login 기능 추가
    - refresh token 작업 미완

---
+++ backend/src/main/java/com/nft/ncity/domain/log/response/LoginPostRes.java
@@ -0,0 +1,23 @@
+package com.nft.ncity.domain.log.response;
+
+import com.nft.ncity.common.model.response.BaseResponseBody;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel(value = "LoginPostRes", description = "유저 로그인시 반환하는 정보")
+public class LoginPostRes extends BaseResponseBody {
+    @ApiModelProperty(name = "JWT 인증 토큰")
+    String accessToken;
+
+    public static LoginPostRes of(Integer statusCode, String message, String accessToken) {
+        LoginPostRes loginRes = new LoginPostRes();
+        loginRes.setStatusCode(statusCode);
+        loginRes.setMessage(message);
+        loginRes.setAccessToken(accessToken);
+        return loginRes;
+    }
+}
---
+++ backend/src/main/java/com/nft/ncity/domain/log/service/LogServiceImpl.java
@@ -0,0 +1,32 @@
+package com.nft.ncity.domain.log.service;
+
+import com.nft.ncity.domain.log.db.repository.LogRepository;
+import com.nft.ncity.domain.user.db.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LogServiceImpl implements LogService{
+
+    @Autowired
+    private LogRepository logRepository;
+
+    @Override
+    public User getUserDetail(String userAddress) {
+        Optional<User> user = logRepository.findUserByUserAddress(userAddress);
+        if(user.isPresent()) {  // 이미 한번 로그인한 유저라면
+            return user.get();
+        } else {    // 처음 로그인한 유저라면
+            User newUser = User.builder()
+                    .userAddress(userAddress)
+                    .userNick("noname") // 닉네임, 코드(일반 회원 : 2) 초기 설정
+                    .userCode(2)
+                    .build();
+            logRepository.save(newUser);    // db에 유저 정보 저장
+            User nowOldUser = logRepository.findUserByUserAddress(userAddress).get();
+            return nowOldUser;
+        }
+    }
+}
