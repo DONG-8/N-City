9ad6b949355538af2f1d92ba63bfa046ff415595
DESKTOP-KVCQHCD\SSAFY <jya3392@naver.com>
2022-03-11 10:26:26 +0900

    [S06P22E106-216] feature-BE : 인증요청내역 전체조회

--- backend/src/main/java/com/nft/ncity/domain/authentication/db/repository/AuthenticationRepository.java
+++ backend/src/main/java/com/nft/ncity/domain/authentication/db/repository/AuthenticationRepository.java
@@ -1,9 +1,11 @@
 package com.nft.ncity.domain.authentication.db.repository;
 
 import com.nft.ncity.domain.authentication.db.entity.Authentication;
+import org.hibernate.sql.Delete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthenticationRepository extends JpaRepository<Authentication, Long> {
+    
 }
