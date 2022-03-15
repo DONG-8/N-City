fde6388e65af59f307493d6997265e7c4dc159c7
박동준 <qkrehdwns96@naver.com>
2022-03-15 10:45:54 +0900

    [S06P22E106-264] Feat : 랭킹페이지 레이아웃 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import { Admin, FAQ, Main, Mypage,NFTStore } from './pages/index';
-import Login from './pages/Login/Login';
-import Apply from './pages/Apply/Apply';
-import GlobalStyle from './styles/global';
-import DetailItem from './pages/NFTStore/DetailItem';
-import SalesResistration from './pages/SalesResistration/SalesResistration';
-import ProfileSetting from './pages/Mypage/ProfileSetting';
-import EventPage from './pages/FAQ/EventPage';
-import EventDetail from './pages/FAQ/EventDetail';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { Admin, FAQ, Main, Mypage, NFTStore } from "./pages/index";
+import Login from "./pages/Login/Login";
+import Apply from "./pages/Apply/Apply";
+import GlobalStyle from "./styles/global";
+import DetailItem from "./pages/NFTStore/DetailItem";
+import SalesResistration from "./pages/SalesResistration/SalesResistration";
+import ProfileSetting from "./pages/Mypage/ProfileSetting";
+import EventPage from "./pages/FAQ/EventPage";
+import EventDetail from "./pages/FAQ/EventDetail";
+import Rank from "./pages/Rank/Rank";
 function App() {
   return (
     <>
@@ -18,19 +19,20 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Main />} />         
-          <Route path="/login" element={<Login />}/>
-          <Route path="/signup" element={<Login />}/>
-          <Route path="/faq" element={<FAQ />}/>
-          <Route path="/admin" element={<Admin />}/>
-          <Route path="/mypage" element={<Mypage />}/>
-          <Route path="/apply" element={<Apply />}/>
-          <Route path="/store" element={<NFTStore />}/>
-          <Route path="/store/detail" element={<DetailItem />}/>
-          <Route path="/SalesResistration" element={<SalesResistration />}/>
-          <Route path="/profilesetting" element={<ProfileSetting />}/>
-          <Route path="/event" element={<EventPage />}/>
-          <Route path="/event/detail" element={<EventDetail />}/>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Login />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/apply" element={<Apply />} />
+          <Route path="/store" element={<NFTStore />} />
+          <Route path="/store/detail" element={<DetailItem />} />
+          <Route path="/SalesResistration" element={<SalesResistration />} />
+          <Route path="/profilesetting" element={<ProfileSetting />} />
+          <Route path="/event" element={<EventPage />} />
+          <Route path="/event/detail" element={<EventDetail />} />
+          <Route path="/rank" element={<Rank />} />
         </Routes>
         <Footer />
       </BrowserRouter>
