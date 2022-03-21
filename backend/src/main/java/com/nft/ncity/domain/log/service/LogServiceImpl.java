fc816600af6b7f189e6f88795bcb06ca011459c6
김지슬 <wltmfdl123@naver.com>
2022-03-22 01:33:16 +0900

    [S06P22E106-329] Fix : 처음 로그인 시 마이룸 초기 정보 생성

--- backend/src/main/java/com/nft/ncity/domain/log/service/LogServiceImpl.java
+++ backend/src/main/java/com/nft/ncity/domain/log/service/LogServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nft.ncity.domain.log.service;
 
 import com.nft.ncity.domain.log.db.repository.LogRepository;
+import com.nft.ncity.domain.myroom.db.entity.MyRoom;
+import com.nft.ncity.domain.myroom.db.repository.MyRoomRepository;
 import com.nft.ncity.domain.user.db.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class LogServiceImpl implements LogService{
     @Autowired
     private LogRepository logRepository;
 
+    @Autowired
+    private MyRoomRepository myRoomRepository;
+
     @Override
     public User getUserDetailByAddress(String userAddress) {
         Optional<User> user = logRepository.findUserByUserAddress(userAddress);
@@ -20,14 +25,22 @@ public class LogServiceImpl implements LogService{
         if(user.isPresent()) {  // 이미 한번 로그인한 유저라면
             return user.get();
         } else {    // 처음 로그인한 유저라면
-            User newUser = User.builder()
+            User newUser = User.builder()   // 유저 정보 빌드
                     .userAddress(userAddress)
                     .userNick("noname") // 닉네임 초기 설정
                     .userRole("ROLE_USER") // 유저 역할 초기 설정 (일반 유저)
                     .userEmailConfirm(false)
                     .build();
-            logRepository.save(newUser);    // db에 유저 정보 저장
-            User nowOldUser = logRepository.findUserByUserAddress(userAddress).get();
+
+            User nowOldUser = logRepository.save(newUser); // db에 유저 정보 저장
+
+            MyRoom newRoom = MyRoom.builder()   // 유저 방 생성
+                    .userId(nowOldUser.getUserId())
+                    .myRoomTodayCnt(0)
+                    .myRoomTotalCnt(0)
+                    .build();
+            myRoomRepository.save(newRoom);
+
             return nowOldUser;
         }
     }
