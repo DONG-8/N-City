88694cc06aaab6ce2657e9afb844217c40a5c28d
김지슬 <wltmfdl123@naver.com>
2022-03-23 18:09:43 +0900

    Fix : 안쓰는 import 삭제

--- backend/src/main/java/com/nft/ncity/common/config/CustomAuthenticationEntryPoint.java
+++ backend/src/main/java/com/nft/ncity/common/config/CustomAuthenticationEntryPoint.java
@@ -3,11 +3,7 @@ package com.nft.ncity.common.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
