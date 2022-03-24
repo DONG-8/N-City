9dff1b67d6f8438af5d95c70692403c676d43fec
박동준 <qkrehdwns96@naver.com>
2022-03-24 15:38:42 +0900

    [S06P22E106-331] Feat : Log API done

---
+++ frontend/src/store/apis/log.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import * as API from "./types" 
+
+const apiClient = axios.create({
+  // baseURL: "https://j6e106.p.ssafy.io/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+
+// 로그 api
+
+// 로그인 요청
+export const postLogin = async (userAddress : string)=> {
+  const response = await apiClient.post<any>(
+    '/users/login',
+    {
+      userAddress
+    }
+  ) 
+  console.log('로그인진행중')
+  return response.data
+}
+
+export const getLogout = async () => {
+  const response = await apiClient.get<any>(
+    '/users/logout',
+  ) 
+  console.log('로그아웃진행중')
+  return response.data
+}
+
