f6f55d694368ff020820cdb008ab0bb11b312f1e
박동준 <qkrehdwns96@naver.com>
2022-03-24 14:43:45 +0900

    [S06P22E106-331] Feat : ProductLike API Done

---
+++ frontend/src/store/apis/favorite.ts
@@ -0,0 +1,56 @@
+import axios from "axios"
+import * as API from "./types" 
+
+const apiClient = axios.create({
+  // baseURL: "https://j6e106.p.ssafy.io/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
+
+// 좋아요 api
+
+// 내가 좋아요 한 여부
+export const getProductLike = async (productId : number) => {
+  const response =await apiClient.get<any>(
+    `/favorites/${productId}`,
+  )
+  console.log('여기와쪄염')
+  return response.data
+}
+
+
+// 상품 좋아요 추가
+export const postProductLike = async (productId : number ) => {
+  const response = await apiClient.post<any>(
+    `/favorites/${productId}`,
+    {
+      productId
+    }
+  )
+  console.log('상품 좋아요 추가 진행중')
+  return response.data
+}
+
+// 좋아요 취소
+export const delProductLike = async (productId : number ) => {
+  const response = await apiClient.delete<any>(
+    `/favorites/${productId}`,
+  )
+  console.log('상품 좋아요 추가 진행중')
+  return response.data
+}
+
+// 좋아요 수 가져오기
+export const getCountProductLike = async (productId : number) => {
+  const response = await apiClient.get<API.TypeLike>(
+    `/favorites/${productId}/count`
+  )
+  return response.data
+}
+
+
+
