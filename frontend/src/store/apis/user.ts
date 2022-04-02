913e61c769a90124f1a48aeb9706f9e85a1f0cc8
최이삭 <isaac950619@gmail.com>
2022-04-03 01:59:02 +0900

    [S06P22E106-322] Chore : 경매 테스트

--- frontend/src/store/apis/user.ts
+++ frontend/src/store/apis/user.ts
@@ -3,16 +3,16 @@ import * as API from "./types"
 
 const apiClient = axios.create({
   // baseURL: "https://j6e106.p.ssafy.io/api",
-  baseURL: "http://localhost:8080/api",
-  // baseURL: "https://j6e106.p.ssafy.io/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "https://j6e106.p.ssafy.io/api",
   headers: {
     "Content-type": "application/json",
   },
 });
 const fileApiClient = axios.create({
   // baseURL: "https://j6e106.p.ssafy.io/api",
-  // baseURL: "https://j6e106.p.ssafy.io/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://j6e106.p.ssafy.io/api",
+  // baseURL: "http://localhost:8080/api",
   headers: {
   'Content-Type': 'multipart/form-data'
   },
