84447f637ab6451a1ef5b3708e666621235b6da1
DESKTOP-KVCQHCD\SSAFY <jya3392@naver.com>
2022-04-04 10:04:20 +0900

    Fix : Email 인증 수정

--- backend/src/main/java/com/nft/ncity/domain/user/service/EmailService.java
+++ backend/src/main/java/com/nft/ncity/domain/user/service/EmailService.java
@@ -18,12 +18,6 @@ class EmailService {
     // 비동기 처리 -> 메일을 보내는동안 블럭상태 방지
     @Async
     public void send(String email, String authToken) {
-//        SimpleMailMessage smm = new SimpleMailMessage();
-//        smm.setTo(email);
-//        smm.setSubject("N-CITY 회원가입 이메일 인증 메일 입니다.");
-//        smm.setText("http://localhost:8080/api/users/confirm-email?email="+email+"&authToken="+authToken);
-
-//        javaMailSender.send(smm);
         MimeMessage message = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
@@ -31,7 +25,8 @@ class EmailService {
             messageHelper.setSubject("N-CITY 회원가입 이메일 인증 메일 입니다.");
             StringBuilder sb = new StringBuilder();
             sb.append("<img  src='https://ncity-bucket.s3.ap-northeast-2.amazonaws.com/forEmailSend/2.png'/> <br>");
-            sb.append("<a href=http://localhost:8080/api/users/confirm-email?email="+email+"&authToken="+authToken+">인증하기</a>");
+//            sb.append("<a href=https://localhost:8080/api/users/confirm-email?email="+email+"&authToken="+authToken+">인증하기</a>");
+            sb.append("<a href=https://j6e106.p.ssafy.io/api/users/confirm-email?email="+email+"&authToken="+authToken+">인증하기</a>");
             messageHelper.setText(sb.toString(),true);
         } catch (Exception e) {
             e.printStackTrace();;
