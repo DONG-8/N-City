827840c02fb88009a21fa9d733dbe61a62bdcbf8
DESKTOP-KVCQHCD\SSAFY <jya3392@naver.com>
2022-04-06 02:42:29 +0900

    Chore : nginx proxy_set_header 추가

--- frontend/src/ingame/services/Network.ts
+++ frontend/src/ingame/services/Network.ts
@@ -31,13 +31,13 @@ export default class Network {
   mySessionId!: string
 
   constructor() { // 네트워크 관련 ⭐⭐⭐⭐
-    // const protocol = window.location.protocol.replace('http', 'ws')
+    const protocol = window.location.protocol.replace('http', 'ws')
     const endpoint =
       // process.env.NODE_ENV === 'production'
       //   ? `wss://sky-office.herokuapp.com`
       //   : `${protocol}//${window.location.hostname}:2567`
-        // `${protocol}//${window.location.hostname}:2567`
-      `wss://j6e106.p.ssafy.io:2567`
+        `${protocol}//${window.location.hostname}:2567`
+      // `ws://j6e106.p.ssafy.io:2567`
     console.log(endpoint, "엔드포인트으으으")
     this.client = new Client(endpoint)
     this.joinLobbyRoom().then(() => {
