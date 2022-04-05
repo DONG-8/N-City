3b1e6dc77fed3e8becd27cd3c0247ada22aaea02
DESKTOP-KVCQHCD\SSAFY <jya3392@naver.com>
2022-04-05 19:01:48 +0900

    Chore : 빌드 수정

--- frontend/src/ingame/services/Network.ts
+++ frontend/src/ingame/services/Network.ts
@@ -36,7 +36,7 @@ export default class Network {
       // process.env.NODE_ENV === 'production'
       //   ? `wss://sky-office.herokuapp.com`
         // : `${protocol}//${window.location.hostname}:2567`
-      "ws://j6e106.p.ssafy.io:2567"
+      "wss://j6e106.p.ssafy.io:2567"
     this.client = new Client(endpoint)
     this.joinLobbyRoom().then(() => {
       store.dispatch(setLobbyJoined(true))
