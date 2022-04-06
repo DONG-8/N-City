5fd0663c00b3f2910f71aafa9214b3d255d02c19
박동준 <qkrehdwns96@naver.com>
2022-04-06 16:24:06 +0900

    [S06P22E106-68] Style : userModal창 Layout 및 토글완료

--- frontend/src/ingame/components/Bar/UIBar.tsx
+++ frontend/src/ingame/components/Bar/UIBar.tsx
@@ -11,6 +11,7 @@ import {
 } from "./style";
 
 import { useMutation, useQuery } from "react-query";
+import { useNavigate, Navigate } from "react-router-dom";
 import { getUserInfo } from "../../../../src/store/apis/user"; //  유저정보 가져오기
 import { useAppSelector, useAppDispatch } from "../../hooks";
 import { getUsercollectedInfo } from "../../../../src/store/apis/user"; //  유저정보 가져오기
@@ -21,6 +22,8 @@ import { Tooltip } from "@mui/material";
 import MusicModal from "../Music/MusicModal";
 import StoreModal from "../NFTstore/StoreModal";
 import VisitModal from "../visitModal";
+import UserModal from "../user/UserModal";
+import { EditModeChange, MakingModeChange } from "../../stores/EditStore";
 
 const UIBar = () => {
   const [musicList, setMusic] = useState();
@@ -30,9 +33,9 @@ const UIBar = () => {
   const [musicTog, setMusicTog] = useState(true);
   const [shopTog, setShopTog] = useState(true);
   const [visitTog, setVisitTog] = useState(true);
-  const toggle = () => {
-    setTog(!tog);
-  };
+  const [userTog, setUserTog] = useState(true);
+
+  const navigation = useNavigate();
 
   const {
     data: Alldata,
@@ -76,6 +79,25 @@ const UIBar = () => {
     }
   );
 
+  // room 이동 요청
+  // get 해당 유저의 id , 데이터 변경 , post 요청 입장, route 주소 변경
+  // Ramdom 로직 확인 후 분할 혹은 병합
+
+  const toggle = () => {
+    setTog(!tog);
+  };
+
+  const gotoHome = () => {
+    // return <Navigate to="/" />;
+    navigation("/");
+    (window as any).game.destroy(true);
+  };
+
+  const ClickModeChange = () => {
+    dispatch(EditModeChange());
+    console.log("변경중");
+  };
+
   const openMusic = () => {
     setMusicTog(!musicTog);
   };
@@ -87,13 +109,17 @@ const UIBar = () => {
   const openVisit = () => {
     setVisitTog(!visitTog);
   };
+
+  const openUser = () => {
+    setUserTog(!userTog);
+  };
   return (
     <Wrapper>
       <Head className={tog ? "close" : "open"}>
         <ToggleBtn onClick={() => toggle()}>
           <img
             className={tog ? "changeButton" : "button"}
-            src="essets/room/arrow-left-circle.png"
+            src="/essets/room/arrow-left-circle.png"
             alt="사진없노"
           ></img>
         </ToggleBtn>
@@ -116,25 +142,30 @@ const UIBar = () => {
           <Absol>{shopTog ? null : <StoreModal></StoreModal>}</Absol>
           <Absol>{visitTog ? null : <VisitModal></VisitModal>}</Absol>
           <Absol>{musicTog ? null : <MusicModal></MusicModal>}</Absol>
+          <Absol>{userTog ? null : <UserModal></UserModal>}</Absol>
           <div
             className="Icon"
             onClick={() => {
               openMusic();
             }}
           >
-            <img className="Mimg" src="essets/room/music.png" alt="사진없노" />
+            <img className="Mimg" src="/essets/room/music.png" alt="사진없노" />
             <div className={tog ? "hidden" : "content"}>
               {musicList && <AudioPlayer tracks={musicList} />}
             </div>
           </div>
-          <div className="Icon">
-            <img className="Mimg" src="essets/room/hammer.png" alt="사진없노" />
+          <div className="Icon" onClick={() => ClickModeChange()}>
+            <img
+              className="Mimg"
+              src="/essets/room/hammer.png"
+              alt="사진없노"
+            />
             <div className={tog ? "hidden" : "content"}>
               <p>Editing</p>
             </div>
           </div>
-          <div className="Icon">
-            <img className="Mimg" src="essets/room/home.png" alt="사진없노" />
+          <div className="Icon" onClick={() => gotoHome()}>
+            <img className="Mimg" src="/essets/room/home.png" alt="사진없노" />
             <div className={tog ? "hidden" : "content"}>
               <p>Home</p>
             </div>
@@ -145,7 +176,7 @@ const UIBar = () => {
               openShop();
             }}
           >
-            <img className="Mimg" src="essets/room/store.png" alt="사진없노" />
+            <img className="Mimg" src="/essets/room/store.png" alt="사진없노" />
             <div className={tog ? "hidden" : "content"}>
               <p>store</p>
             </div>
@@ -156,7 +187,7 @@ const UIBar = () => {
               openVisit();
             }}
           >
-            <img className="Mimg" src="essets/room/visit.png" alt="사진없노" />
+            <img className="Mimg" src="/essets/room/visit.png" alt="사진없노" />
             <div className={tog ? "hidden" : "content"}>
               <p>Guest Book</p>
             </div>
@@ -164,10 +195,10 @@ const UIBar = () => {
           <div
             className="Icon"
             onClick={() => {
-              openVisit();
+              openUser();
             }}
           >
-            <img className="Mimg" src="essets/room/visit.png" alt="사진없노" />
+            <img className="Mimg" src="/essets/room/visit.png" alt="사진없노" />
             <div className={tog ? "hidden" : "content"}>
               <p>UserList</p>
             </div>
@@ -175,16 +206,16 @@ const UIBar = () => {
         </Body>
         <BottomItem className={tog ? "close" : "open"}>
           <div className="Bottom">
-            <img src="essets/room/move.png" alt="사진없노" />
+            <img src="/essets/room/move.png" alt="사진없노" />
             <div className={tog ? "hidden" : "content"}>
               <Tooltip title="Random User">
                 <button>
-                  <img className="user" src="essets/room/random.png" alt="" />
+                  <img className="user" src="/essets/room/random.png" alt="" />
                 </button>
               </Tooltip>
               <Tooltip title="Search User">
                 <button>
-                  <img className="user" src="essets/room/search.png" alt="" />
+                  <img className="user" src="/essets/room/search.png" alt="" />
                 </button>
               </Tooltip>
             </div>
