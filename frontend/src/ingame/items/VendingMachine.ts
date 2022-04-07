9b5313d42453b0685bc9658609ab1a99c88f6c2f
bbnerino <bbnerino@gmail.com>
2022-04-07 17:57:32 +0900

    [S06P22E106-381] Feat : 사진 상세 보기 기능 추가

--- frontend/src/ingame/items/VendingMachine.ts
+++ frontend/src/ingame/items/VendingMachine.ts
@@ -2,7 +2,7 @@ import Item from './Item'
 import { ItemType } from './Item'
 import store from '../stores'
 
-import { openVendingMachineDialog } from '../stores/VendingMachineStore'
+import { openVendingMachineDialog, setProductNum } from '../stores/VendingMachineStore'
 
 export default class VendingMachine extends Item {
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
@@ -15,6 +15,9 @@ export default class VendingMachine extends Item {
     this.setDialogBox('R-그림 자세히 보기')
   }
   openDialog(productId){
+    store.dispatch(setProductNum(productId))
     store.dispatch(openVendingMachineDialog(Number(productId)))
+    // sessionStorage.setItem('productId',productId)
+    console.log('productId',productId)
   }
 }
--- frontend/src/ingame/stores/VendingMachineStore.ts
+++ frontend/src/ingame/stores/VendingMachineStore.ts
@@ -3,11 +3,13 @@ import { createSlice,PayloadAction } from '@reduxjs/toolkit'
 interface VendingMachineState{
   vendingMachineDialogOpen:boolean
   nftnumber:Number
+  productNum:Number
 }
 
 const initialState : VendingMachineState={
   vendingMachineDialogOpen : false,
-  nftnumber:1
+  nftnumber:1,
+  productNum:1,
 }
 
 export const vendingMachineSlice = createSlice({
@@ -18,12 +20,15 @@ export const vendingMachineSlice = createSlice({
         state.nftnumber = action.payload
         state.vendingMachineDialogOpen = true
       },
-    closeVendingMachineDialogOpen:(state)=>{state.vendingMachineDialogOpen = false}
+    closeVendingMachineDialogOpen:(state)=>{state.vendingMachineDialogOpen = false},
+    setProductNum :(state,action:PayloadAction<Number>)=>
+    {state.productNum = action.payload }
   }
 })
 export const {
   openVendingMachineDialog,
-  closeVendingMachineDialogOpen
+  closeVendingMachineDialogOpen,
+  setProductNum
 } = vendingMachineSlice.actions
 
 export default vendingMachineSlice.reducer
