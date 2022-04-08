f0b8cf29fc7431c25db2713296c9b3171882d125
박동준 <qkrehdwns96@naver.com>
2022-04-08 09:25:21 +0900

    [S06P22E106-68] Fix : 방명록 수정버튼 작성자 제외처리 fix

--- frontend/src/ingame/components/visitModal.tsx
+++ frontend/src/ingame/components/visitModal.tsx
@@ -119,7 +119,7 @@ const VisitModal = () => {
   const postBook = useMutation<any, Error>(
     "postBookinput",
     async () => {
-      return await postGuestBook(inputValue, ownerId, myid);
+      return await postGuestBook(inputValue, myid, ownerId);
     },
     {
       onSuccess: (res) => {
